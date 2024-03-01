be5bf2be5e5aee79e6fbd75cb0597f2f729c156a
2xTi <treisalmeida@gmail.com>
2024-03-01 20:40:01 -0300

    first commit

---
+++ lab5quest1/src/main/java/com/mycompany/lab5quest1/Lab5quest1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab5quest1;
+
+/**
+ *
+ * @author palit
+ */
+public class Lab5quest1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
